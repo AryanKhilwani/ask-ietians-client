6853e83ae8b85cb49e13bf837374e8a71b7f22fa
akshat <akshat7509999412@gmail.com>
2022-10-05 19:41:32 +0530

    added photo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -142,7 +142,7 @@ const ResponsiveAppBar = () => {
 
                 <Tooltip title="Open settings">
                   <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                    <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                    <Avatar alt={user.displayName} src={user.photoURL} />
                   </IconButton>
                 </Tooltip>
                 <Menu
